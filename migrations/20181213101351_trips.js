d3bc9064e31c567a90f1dfff67ea3f2b17c08745
Jake Mosher <jakemo136@gmail.com>
2018-12-13 10:45:25 -0700

    trips migrate done

--- migrations/20181213101351_trips.js
+++ migrations/20181213101351_trips.js
@@ -1,8 +1,15 @@
 
-exports.up = function(knex, Promise) {
-  
-};
+exports.up = (knex) => {
+  return knex.schema.createTable('trips', (table) => {
+    table.integer('id')
+    table.integer('busId').references('buses.id').onDelete('CASCADE')
+    table.integer('eventId').references('events.id').onDelete('CASCADE')
+    table.integer('pickupLocationId').references('pickup_locations.id').onDelete('CASCADE')
+    table.integer('driverId').references('users.id').onDelete('CASCADE')
+    table.time('departureTime').notNullable().defaultsTo('18:00:00')
+  })
+}
 
-exports.down = function(knex, Promise) {
-  
-};
+exports.down = (knex) => {
+  return knex.schema.dropTable("trips") 
+}
