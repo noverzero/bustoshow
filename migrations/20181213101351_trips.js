36807bcaef2f466f61dc3192924de5e547dd8ba0
Jake Mosher <jakemo136@gmail.com>
2018-12-13 10:56:34 -0700

    all .onDelete('CASCADE') entries removed

--- migrations/20181213101351_trips.js
+++ migrations/20181213101351_trips.js
@@ -2,10 +2,10 @@
 exports.up = (knex) => {
   return knex.schema.createTable('trips', (table) => {
     table.integer('id')
-    table.integer('busId').references('buses.id').onDelete('CASCADE')
-    table.integer('eventId').references('events.id').onDelete('CASCADE')
-    table.integer('pickupLocationId').references('pickup_locations.id').onDelete('CASCADE')
-    table.integer('driverId').references('users.id').onDelete('CASCADE')
+    table.integer('busId').references('buses.id')
+    table.integer('eventId').references('events.id')
+    table.integer('pickupLocationId').references('pickup_locations.id')
+    table.integer('driverId').references('users.id')
     table.time('departureTime').notNullable().defaultsTo('18:00:00')
   })
 }
--- migrations/20181213101401_reservations.js
+++ migrations/20181213101401_reservations.js
@@ -2,8 +2,8 @@
 exports.up = (knex) => {
   return knex.schema.createTable('reservations', (table) => { 
     table.increments('id')
-    table.integer('userId').references('users.id').onDelete('CASCADE')
-    table.integer('tripId').references('trips.id').onDelete('CASCADE')
+    table.integer('userId').references('users.id')
+    table.integer('tripId').references('trips.id')
     table.boolean('isPaid').notNullable().defaultsTo('false')
     table.boolean('isFavorite').notNullable().defaultsTo('false')
   })
