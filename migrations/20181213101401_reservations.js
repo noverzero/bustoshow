9d95a89967c76cea7158ad8d2b7b9bb8d45a95cc
Jake Mosher <jakemo136@gmail.com>
2018-12-13 10:51:17 -0700

    reservations migrate done

--- migrations/20181213101401_reservations.js
+++ migrations/20181213101401_reservations.js
@@ -1,8 +1,13 @@
 
-exports.up = function(knex, Promise) {
-  
-};
+exports.up = (knex) => {
+  return knex.schema.createTable('reservations', (table) => { 
+    table.increments('id')
+    table.integer('userId').references('users.id').onDelete('CASCADE')
+    table.integer('tripId').references('trips.id').onDelete('CASCADE')
+    table.boolean('isPaid').notNullable().defaultsTo('false')
+    table.boolean('isFavorite').notNullable().defaultsTo('false')
+}
 
-exports.down = function(knex, Promise) {
-  
-};
+exports.down = (knex) => {
+  return knex.schema.dropTable("reservations") 
+}
