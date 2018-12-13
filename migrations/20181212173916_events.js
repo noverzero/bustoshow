e71322a105e357a60d8b4e47774ec717d02b018f
blucky36 <blucky36@gmail.com>
2018-12-13 10:15:49 -0700

    events migration made and extra db seeds and migrations generated

--- migrations/20181212173916_events.js
+++ migrations/20181212173916_events.js
@@ -1,8 +1,14 @@
+exports.up = (knex) => {
+  return knex.schema.createTable("events",(table)=>{
+    table.increments()
+    table.string("venue").notNullable().defaultTo("")
+    table.string("headliner").notNullable().defaultsTo("")
+    table.date("date").notNullable()
+    table.time("startTime").notNullable()
+    table.timestamps(true,true)
+  })
+}
 
-exports.up = function(knex, Promise) {
-  
-};
-
-exports.down = function(knex, Promise) {
-  
-};
+exports.down = (knex) => {
+  return knex.schema.dropTable("events")
+}
