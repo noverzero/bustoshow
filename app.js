62a40321bc39196c738b4e51a15e60f669ae2c6e
Jake Mosher <jakemo136@gmail.com>
2018-12-13 13:44:38 -0700

    dummy seed data in USERS seed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const {util, seed} = require('data-seed')
 const indexRouter = require('./mvc/routes/routes');
 
 const app = express();
--- seeds/001_events.js
+++ seeds/001_events.js
@@ -1,13 +1,16 @@
 
-exports.seed = function(knex, Promise) {
+exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('table_nameTable').del()
     .then(function () {
       // Inserts seed entries
       return knex('table_name').insert([
         {id: 1, colName: 'rowValue1'},
         {id: 2, colName: 'rowValue2'},
         {id: 3, colName: 'rowValue3'}
-      ]);
-    });
-};
+      ])
+    })
+    .then(() => {
+      return knex.raw("SELECT setval('events_id_seq', (SELECT MAX(id) FROM events))")
+    })
+}
