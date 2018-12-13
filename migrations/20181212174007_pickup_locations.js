dd755fe1434997e4dc8abc65b1494c8c2ec5b2d4
Jake Mosher <jakemo136@gmail.com>
2018-12-13 11:11:07 -0700

    yelling at me to commit

--- migrations/20181212174007_pickup_locations.js
+++ migrations/20181212174007_pickup_locations.js
@@ -1,9 +1,9 @@
 
 exports.up = (knex) => {
   return knex.schema.createTable("pickupLocations",(table)=>{
-   table.increments()
-   table.string("streetAddress").notNullable()
-   table.string("locationName").notNullable()
+    table.increments()
+    table.string("streetAddress").notNullable()
+    table.string("locationName").notNullable()
 }
 
 exports.down = (knex) => {
