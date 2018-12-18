f403db083f1c7a6696d538e9c82aec25f7e712e1
blucky36 <blucky36@gmail.com>
2018-12-18 10:08:17 -0700

    working on all reservations for a single user

--- mvc/model/reservationsModel.js
+++ mvc/model/reservationsModel.js
@@ -15,8 +15,23 @@ const getAllR = () => {
 }
 const getOneR = (resId) => {
   if(!resId){
-
+    return {error: "error snake returns"}
   }
+  return knex("events").select("date","headliner","startTime")
+  .innerJoin("trips","events.id","trips.eventId").select("*")
+  .innerJoin("pickup_locations","pickup_locations.id","trips.pickupLocationId")
+  .innerJoin("reservations","reservations.tripId","trips.id").select("*").where("reservations.id",resId)
+  .innerJoin("users","users.id","reservations.userId").then((bigAssObjectArray)=>{
+    return bigAssObjectArray.reduce((whereTheseObjectsAreAccumulating,singleObjectFromBigAssObjectArray)=>{
+      const {email,firstName,lastName,venue,headliner,streetAddress,locationName,date,departureTime} = singleObjectFromBigAssObjectArray
+      whereTheseObjectsAreAccumulating.push({email,firstName,lastName,venue,headliner,streetAddress,locationName,date,departureTime})
+      return whereTheseObjectsAreAccumulating
+    },[])//quad join woot blake loves you
+  })
+}
+
+const getAllByUser = (userId) => {
+
 }
 const createR = () => {
 
@@ -32,5 +47,6 @@ module.exports = {
   getOneR,
   createR,
   updateR,
-  deleteR
+  deleteR,
+  getAllByUser
 }
